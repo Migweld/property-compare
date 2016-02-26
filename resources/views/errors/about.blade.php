e546f2ddd632025aa2b82497248ea2b8af59e22b
Craig Thompson <craig@flodesign.co.uk>
2016-02-26 08:49:27 +0000

    Added about page

---
+++ resources/views/errors/about.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+@section('content')
+          <span class='leeds bg-text purple'>
+            Leeds
+          </span>
+        <div class='box box--about'>
+            <h1>About</h1>
+            <p>
+                Leedsvlondon.com is a small side project we made for fun inspired by the
+                <a href='http://www.londontoleeds.com/'>London to Leeds</a>
+                initiative. We built this in around 2 days, it has absolutely not been fully tested and it may well break so soz if it does.
+            </p>
+            <p>This site was made for a bit of a laugh, it is NOT a serious property searching tool and you should absolutely not use it to find your next home. We know we're not compairing apples with apples, please do not send us tweets or emails about the results you get. Do however have a chuckle at how ludicrously over priced London property is. Cheers!</p>
+            <p>
+                Much love
+                <a href='https://twitter.com/russpoulter'>Russ</a>
+                &
+                <a href='https://twitter.com/migweld'>Craig</a>
+                from
+                <a href='https://flodesign.co.uk/'>Flo Design</a>
+            </p>
+        </div>
+          <span class='london bg-text mint'>
+            London
+          </span>
+@stop
